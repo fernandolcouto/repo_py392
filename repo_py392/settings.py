d67dec5982674aed0f911491891a1e9d93b396b5
fernandolcouto <fernandolcouto@gmail.com>
2021-03-23 10:07:47 -0300

    Resolve Issue #2: heroku config:set SECRET_KEY

--- repo_py392/settings.py
+++ repo_py392/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3qgqhcao(h(6^ukwe+*p@3+2xv5jw3@-&vg2(jy)7u0cu^ecng'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool)
